541fd0686e4e3a882e701c91cf976438c1440465
Saeyeon0 <100344816+Saeyeon0@users.noreply.github.com>
2022-12-19 02:29:27 +0600

    Add files via upload

---
+++ homepage/home.js
@@ -0,0 +1,37 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modals = document.getElementById("Modal");
+var btn = document.getElementById("myButton");
+
+var span = document.getElementsByClassName("closed")[0];
+
+btn.onclick = function () {
+  modals.style.display = "block";
+}
+
+span.onclick = function () {
+  modals.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modals.style.display = "none";
+  }
+}
